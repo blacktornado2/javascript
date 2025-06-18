cabe607755205cf2a9c35d6dce6e9a3b9d228de4
ankitb <ankitb@one.com>
2025-06-18 09:40:46 +0530

    added reduce method

--- practice.js
+++ practice.js
@@ -13,7 +13,7 @@ function formLargestNumber(arr) {
 }
 
 const input = [3, 30, 9, 34, 5, 9];
-formLargestNumber(input);
+// formLargestNumber(input);
 
 
 // const arr = ["2", "100", "9"];
@@ -23,6 +23,19 @@ formLargestNumber(input);
 // console.log("Lexical Ascending:", sortedAscending); // ["100", "2", "9"]
 
 
-setTimeout(function () {
-    console.log("This is a delayed message");
-}, 2000);
+// setTimeout(function () {
+//     console.log("This is a delayed message");
+// }, 2000);
+
+
+// Reduce: very versatile method, can be used as a forEach, map, filter, and more
+// 1st param -> callback function, 2nd param -> Initial value of accumulator
+// return the new value of acc
+
+// Find sum of all elements in array
+input.reduce((acc, curr) => {
+    return acc + curr;
+}, 0);
+
+// Find max value in array
+input.reduce((acc, curr) => Math.max(acc, curr), -Infinity);
